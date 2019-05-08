1e60c058f1759ff9ff9ded003a59b59d1ecfb02c
Andrea Veneziano <andrea.veneziano@epfl.ch>
2019-05-08 12:01:09 +0200

    Add queries 12 and 11

--- queries-deliv3.sql
+++ queries-deliv3.sql
@@ -23,3 +23,20 @@ FROM Listing L, Reviews R
 WHERE L.lid = R.lid
 GROUP BY L.hid
 ORDER BY COUNT(L.lid) DESC LIMIT 3
+
+SELECT DISTINCT Co.coid, Co.country
+FROM Country Co, Listing L
+WHERE
+(SELECT COUNT(L.lid)
+FROM Calendar Ca
+WHERE Co.coid = L.coid AND Ca.lid = L.lid AND Ca.available = 't' AND Ca.cdate >= '20180101' AND Ca.cdate <= '20181231') >=
+    (SELECT COUNT(L.lid) * 20 / 100
+    WHERE Co.coid = L.coid)
+
+SELECT N.nid, N.neighbourhood
+FROM Neighbourhood N
+WHERE
+    (SELECT COUNT(N.nid)
+    FROM Listing L, City C, Cancellation_policy CP
+    WHERE L.nid = N.nid AND L.ciid = C.ciid AND C.city = 'Barcelona' AND L.cpid = CP.cpid AND CP.cancellation_policy = 'strict_14_with_grace_period') > (SELECT COUNT(N.nid) * 5 / 100 FROM Listing L, City C
+    WHERE L.nid = N.nid AND L.ciid = C.ciid AND C.city = 'Barcelona')
